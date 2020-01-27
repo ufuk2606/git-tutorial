512db09f7f5de33ba00a22e2eff40cf33f608ffd
ufuk2606 <ufukozkar11@gmail.com>
2020-01-27 23:10:38 +0100

    insaatMuhendisi

---
+++ Ufukk/insaat_muhendisi/Ufuk_Ozkar_JS_103_insaat_muhendisi/app.js
@@ -0,0 +1,26 @@
+
+
+let daireA = daireAlan(2);
+console.log(daireA);
+
+
+let dikdortgenA = dikdortgenAlan(2, 5);
+console.log(dikdortgenA);
+
+
+let daireC = daireCevre(2);
+console.log(daireC);
+
+
+let dikdortgenC = dikdortgenCevre (2,3) ;
+console.log(dikdortgenC);
+
+
+let kupH = kupHacim(3);
+console.log(kupH);
+
+
+let kupA = kupAlan (3);
+console.log(kupA);
+
+
---
+++ Ufukk/insaat_muhendisi/Ufuk_Ozkar_JS_103_insaat_muhendisi/hesaplama_metodlari.js
@@ -0,0 +1,41 @@
+
+
+function daireAlan(r) {
+    let sayi = Math. round(Math.PI)  * Math.pow(r, 2);
+    return sayi;
+}
+
+
+function dikdortgenAlan(a, b) {
+    let sayi = a * b;
+    return sayi;
+}
+
+
+function daireCevre(r) {
+    let sayi = 2 * Math. round(Math.PI) * r;
+    return sayi;
+}
+
+
+function dikdortgenCevre(a, b) {
+    let sayi = 2 * (a + b);
+    return sayi;
+}
+
+
+function kupHacim(x) {
+    let sayi = Math.pow(x,3);
+    return sayi;
+}
+
+
+function kupAlan(x) {
+    let sayi = 6 * Math.pow(x,2);
+    return sayi;
+}
+
+
+
+
+

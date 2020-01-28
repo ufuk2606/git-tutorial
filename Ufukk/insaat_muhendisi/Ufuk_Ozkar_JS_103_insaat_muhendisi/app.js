a445fcd01667356e807ed3e581bc42a1eb103f0d
ufuk2606 <ufukozkar11@gmail.com>
2020-01-28 21:12:09 +0100

    InsaatMuhendisiSon

--- Ufukk/insaat_muhendisi/Ufuk_Ozkar_JS_103_insaat_muhendisi/app.js
+++ Ufukk/insaat_muhendisi/Ufuk_Ozkar_JS_103_insaat_muhendisi/app.js
@@ -1,26 +1,26 @@
 
 
 let daireA = daireAlan(2);
-console.log(daireA);
+console.log("Dairenin Alanı = " , daireA);
 
 
 let dikdortgenA = dikdortgenAlan(2, 5);
-console.log(dikdortgenA);
+console.log("Dikdortgenin Alanı = " , dikdortgenA);
 
 
 let daireC = daireCevre(2);
-console.log(daireC);
+console.log("Dairenin Cevresi = " ,daireC);
 
 
 let dikdortgenC = dikdortgenCevre (2,3) ;
-console.log(dikdortgenC);
+console.log("Dikdortgenin Cevresi = " ,dikdortgenC);
 
 
 let kupH = kupHacim(3);
-console.log(kupH);
+console.log("Kupun Hacmi = " ,kupH);
 
 
 let kupA = kupAlan (3);
-console.log(kupA);
+console.log("Kupun Alani = " ,kupA);
  
 

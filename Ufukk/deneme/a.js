ecf188771350089cdb11fb045e068a2c41a454e9
ufuk2606 <ufukozkar11@gmail.com>
2020-01-31 11:33:11 +0100

    denemeler vardı

--- Ufukk/deneme/a.js
+++ Ufukk/deneme/a.js
@@ -1,14 +1,3 @@
-let sayilar1 = [ 0, 1, 12, 3, 14, 5];
-let sayilar2 = [ 10, 11, 12, 9, 14, 5, 28];
-let dizi = [];
+let kelime = '       Ufuk Ozkar bu odevi titizlikle yapıyor.       ';
 
-for (let i = 0; i < sayilar1.length; i++) {
-    
-    for (let j = 0; j < sayilar2.length; j++) {
-        
-        if ( sayilar1[i] === sayilar2[j] ) {
-            dizi.push(sayilar1[i]);
-        }
-    }
-}
-console.log(dizi);
+console.log(kelime.trimStart());

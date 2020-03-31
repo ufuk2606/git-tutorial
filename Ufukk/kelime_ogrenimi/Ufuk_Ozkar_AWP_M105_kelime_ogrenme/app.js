5e1e1e2e70719e22f5369c418ce1de75e9a95fbe
ufuk2606 <ufukozkar11@gmail.com>
2020-03-31 20:21:33 +0200

    Kelime_ogrenimi_aciklamalar

--- Ufukk/kelime_ogrenimi/Ufuk_Ozkar_AWP_M105_kelime_ogrenme/app.js
+++ Ufukk/kelime_ogrenimi/Ufuk_Ozkar_AWP_M105_kelime_ogrenme/app.js
@@ -1,1 +1,18 @@
+/**
+ * Array liste olusturulacak.Almanca kelimeler olacak.
+ * Bir liste daha olacak Turkce karsiliklari olacak
+ * Bu ikisi bir clss ta tutulacak
+ * Listeden bir kelime random bir sekilde innerHTML sine yazilacak
+ * Ve altinda 4 tane cevap sikki.Siklar rastgele yerlestirilecek
+ * Bunlari <li> seklinde yazarsak vu <ul> ye EventListener eklersek iyi olur
+ * Bu listelere tiklandiginda butun cevaplar gozukecek. Dogru sayisi ve Yanlis Sayisi guncellenecek.
+ * bir buton koyup sonraki kelimeye ilerle. 
+ */
+
 almancaKelimeyiYazdir();
+
+/**
+ * class modellemeye biraz kafa yordum. Zaten onu da tam yapamadim. CSS e vakit kalmadi. 
+ * Hic gondermemektense yarim halini gondermek istedim. 
+ * Ama burada yazdigim gibi konustugum gibi kodlari yazamiyorum. Problemler cikiyor. 
+ */

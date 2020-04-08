ba0fceea5190ca59cff9a7297e2a252e6dc32bad
ufuk2606 <ufukozkar11@gmail.com>
2020-04-08 15:09:03 +0200

    Muhasebeci_Leyla_Hanim

--- Ufukk/Muhasebeci_Leyla_Hanım/ufuk_ozkar_AWP_M105_muhasebeci/app.js
+++ Ufukk/Muhasebeci_Leyla_Hanım/ufuk_ozkar_AWP_M105_muhasebeci/app.js
@@ -1,5 +1,14 @@
-
-
+/**
+ * Bu applikasyon, yeni kisi kaydeder. Kayitli kisileri tabloda listeler ve tabloda siralama ve filtreleme secenegi de sunar.
+ * 
+ * Applikasyon akisi
+ *   - Kullanici solda bulunan formdan isim, soyisim ve kanton bilgilerini girer.
+ *   - Bilgileri girdikten sonra Kaydet butonuna tiklar ve bu bilgiler tabloda yazdirilir.
+ *   - Tiklama isleminden sonra form temizlenir.
+ *   - Listelenen tabloda isim, soyisim ve kantona gore siralama yapilabilir. (Hem a'dan z'ye hem de z'den a'ya)
+ *   - Isim veya soyisime gore filtreleme islemi yapilabilir.
+ */
+let statuler = new Statuler();
 let kisi = new Kisi();
 let kanton = new Kanton();
 let siralama = new Siralama();
@@ -7,4 +16,7 @@ let arama = new Arama();
 let dom = new Dom();
 let manager = new Manager();
 
+/**
+ * Programi baslatir
+ */
 manager.start();
--- Ufukk/Muhasebeci_Leyla_Hanım/ufuk_ozkar_AWP_M105_muhasebeci/model.js
+++ Ufukk/Muhasebeci_Leyla_Hanım/ufuk_ozkar_AWP_M105_muhasebeci/model.js
@@ -1,6 +1,3 @@
-let isimSiralamaninStatusu = true;
-let soyisimSiralamaninStatusu = true ;
-let kantonSiralamaninStatusu = true;
 
 class Kisi{
     constructor(isim, soyisim, kanton){
@@ -9,7 +6,11 @@ class Kisi{
         this.kanton = kanton;
         this.kisiler = [];
     };
-
+    /**
+     * Bu fonksiyon, Kaydet butonuna tiklandiginda formdaki bilgilerle yeni bir kisi olusturur.
+     * Kayit sonrasi formu temizler
+     * Kayitli kisileri tabloya yazdirir.
+     */
     yeniKisiOlustur(){
         let buton = dom.verilenIdyiYakala("buton");
         buton.addEventListener("click", function() {
@@ -22,13 +23,18 @@ class Kisi{
             this.tabloyaYazdir(this.kisiler); 
         }.bind(this));
     }
-
+    /**
+     * Bu fonksiyon formu temizler.
+     */
     formuTemizle(){
         dom.verilenIdyiYakala("isim").value = "" ;
         dom.verilenIdyiYakala("soyisim").value = "" ;
         dom.verilenIdyiYakala("kanton").value = "" ;
     }
-
+    /**
+     * Bu fonksiyon kendine gonderilen parametredeki kisinin isim, soyisim ve kantona gore ilgili alanlara yazdirir.
+     * @param {*} pKisi 
+     */
     tabloyaYazdir(pKisi){
         let tbody = dom.verilenIdyiYakala("tbody");
         let yazilacak = pKisi.map(item =>{
@@ -66,7 +72,9 @@ class Kanton{
         { isim : "Jura"}
         ]
     }
-
+    /**
+     * Bu fonksiyon Dom daki ilgili yere kantonlari yazdirir.
+     */
     kantonlariYazdir(){
         let kanton =dom.verilenIdyiYakala("kanton");
         let yazilacakKantonlar = this.kantonlar.map(item =>{
@@ -78,24 +86,29 @@ class Kanton{
 }
 
 class Siralama{
+    /**
+     * Bu fonksiyon tablodaki isim, soyisim ve kanton bilgilerini (A dan Z ye ve tersine) siralama yapar.
+     */
     tabloSiralama(){
         let tablo = dom.verilenIdyiYakala("tablo");
         tablo.addEventListener("click", function(e) {
             if(e.target.id === "isimSiralama"){
-                isimSiralamaninStatusu ? this.AdanZyeIsimleriSirala() : this.ZdenAyaIsimleriSirala() ;
-                isimSiralamaninStatusu = !isimSiralamaninStatusu ;
+                statuler.isimSiralamaninStatusu ? this.AdanZyeIsimleriSirala() : this.ZdenAyaIsimleriSirala() ;
+                statuler.isimSiralamaninStatusu = !statuler.isimSiralamaninStatusu ;
             }
             if(e.target.id === "soyisimSiralama"){
-                soyisimSiralamaninStatusu ? this.AdanZyeSoyisimleriSirala() : this.ZdenAyaSoyisimleriSirala() ;
-                soyisimSiralamaninStatusu = !soyisimSiralamaninStatusu ;
+                statuler.soyisimSiralamaninStatusu ? this.AdanZyeSoyisimleriSirala() : this.ZdenAyaSoyisimleriSirala() ;
+                statuler.soyisimSiralamaninStatusu = !statuler.soyisimSiralamaninStatusu ;
             }
             if(e.target.id === "kantonSiralama"){
-                kantonSiralamaninStatusu ? this.AdanZyeKantonlariSirala() : this.ZdenAyaKantonlariSirala() ;
-                kantonSiralamaninStatusu = !kantonSiralamaninStatusu ;
+                statuler.kantonSiralamaninStatusu ? this.AdanZyeKantonlariSirala() : this.ZdenAyaKantonlariSirala() ;
+                statuler.kantonSiralamaninStatusu = !statuler.kantonSiralamaninStatusu ;
             }
         }.bind(this));
     }
-
+    /**
+     * A dan Z ye isimleri siralar.
+     */
     AdanZyeIsimleriSirala() {
         kisi.kisiler = kisi.kisiler.sort(function(a, b){
             var x = a.isim.toLowerCase();
@@ -106,6 +119,9 @@ class Siralama{
         });
         kisi.tabloyaYazdir(kisi.kisiler);
     }
+    /**
+     * Z den A ya isimlari siralar.
+     */
     ZdenAyaIsimleriSirala() {
         kisi.kisiler = kisi.kisiler.sort(function(a, b){
             var x = b.isim.toLowerCase();
@@ -116,6 +132,9 @@ class Siralama{
         });
         kisi.tabloyaYazdir(kisi.kisiler);
     }
+    /**
+     * A dan Z ye soyisimleri siralar.
+     */
     AdanZyeSoyisimleriSirala() {
         kisi.kisiler = kisi.kisiler.sort(function(a, b){
             var x = a.soyisim.toLowerCase();
@@ -126,6 +145,9 @@ class Siralama{
         });
         kisi.tabloyaYazdir(kisi.kisiler);
     }
+    /**
+     * Z den A ya soyisimlari siralar.
+     */
     ZdenAyaSoyisimleriSirala() {
         kisi.kisiler = kisi.kisiler.sort(function(a, b){
             var x = b.soyisim.toLowerCase();
@@ -136,6 +158,9 @@ class Siralama{
         });
         kisi.tabloyaYazdir(kisi.kisiler);
     }
+    /**
+     * A dan Z ye kantonlari siralar.
+     */
     AdanZyeKantonlariSirala() {
         kisi.kisiler = kisi.kisiler.sort(function(a, b){
             var x = a.kanton.toLowerCase();
@@ -146,6 +171,9 @@ class Siralama{
         });
         kisi.tabloyaYazdir(kisi.kisiler);
     }
+    /**
+     * Z den A ya kantonlari siralar.
+     */
     ZdenAyaKantonlariSirala() {
         kisi.kisiler = kisi.kisiler.sort(function(a, b){
             var x = b.kanton.toLowerCase();
@@ -159,6 +187,9 @@ class Siralama{
 }
 
 class Arama{
+    /**
+     * Bu fonksiyon, arama kutusuna girilen karakter veya kelimelere gore isim ve soyisim sütunlarında filtreleme yapar.
+     */
     filtreleme(){
         let aramaBolumu = dom.verilenIdyiYakala("tablonun-ustunun-sagi");
         aramaBolumu.addEventListener("click", function(e) {
@@ -176,8 +207,20 @@ class Arama{
 }
 
 class Dom{
+    /**
+     * Bu fonksiyon kendine gonderilen parametreyle Html de ilgili id yi yakalar.
+     * @param {*} pId 
+     */
     verilenIdyiYakala(pId){
         return document.querySelector(`#${pId}`);
+    }
+}
+
+class Statuler{
+    constructor(){
+        this.isimSiralamaninStatusu = true;
+        this.soyisimSiralamaninStatusu = true ;
+        this.kantonSiralamaninStatusu = true;
     }
 }
 
